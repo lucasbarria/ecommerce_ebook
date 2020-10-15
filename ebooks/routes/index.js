002b04fea2497f2c1d8cabda159411c0dac7ab3a
ulisesmorgulis <ulisesmorgulis.99@gmail.com>
2020-10-15 15:54:58 -0300

    home

--- ebooks/routes/index.js
+++ ebooks/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render("'index', { title: 'Express' }");
+  res.render("home");
 });
 
 router.get('/paginaDeProducto', function(req, res, next) {
@@ -18,12 +18,8 @@ router.get('/iniciasesion', function(req, res, next) {
   res.render("iniciasesion");
 });
 
-<<<<<<< HEAD
 router.get('/carrito', function(req, res, next) {
   res.render("productCart");
-=======
-router.get('/home', function(req, res, next) {
-  res.render("home");
->>>>>>> 10bd0776b59504716a8db4f27792d285e5e078dc
 });
-module.exports = router;
+
+module.exports = router; 
