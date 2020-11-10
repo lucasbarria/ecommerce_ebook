bb1a0e75b7d9e27e3101421201673ac19ddb8bb9
ulisesmorgulis <ulisesmorgulis.99@gmail.com>
2020-11-10 17:23:24 -0300

    crearproducto

--- ebooks/controllers/productController.js
+++ ebooks/controllers/productController.js
@@ -1,7 +1,16 @@
 const productController = {
     crearProducto : function (req, res) {
-        res.render ('crearProducto');
+        res.render('crearProducto');
     },
+    create: function (req, res) {
+        let producto = {
+            nombre: req.body.nombre,
+            descripcion: req.body.descripcion,
+            imagen: req.body.img,
+            categoria: req.body.categoria,
+            precio: req.body.precio
+        }
+    }
 }
 
 module.exports = productController;
