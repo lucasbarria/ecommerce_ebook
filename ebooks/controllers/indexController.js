cbcd98544ae9c29b6192f4850c4f71f3e03b83cc
lucas barria <lucasbarria2002@gmail.com>
2020-11-10 16:07:40 -0300

    controller

--- ebooks/controllers/indexController.js
+++ ebooks/controllers/indexController.js
@@ -1,5 +1,19 @@
 const indexController = {
     home: function(req, res, next) {
-        res.render("home");
-      };
+        res.render('home');
+      },
+    productDetail: function(req, res, next) {
+        res.render("productDetail");
+      },
+    register: function(req, res, next) {
+        res.render("register");
+      },
+    iniciasesion: function(req, res, next) {
+        res.render("iniciasesion");
+      },
+    productCart: function(req, res, next) {
+        res.render("productCart");
+      }
 }
+
+module.exports = indexController
