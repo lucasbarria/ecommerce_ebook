e92fa3d08ae963e2cc7f0ea6cfefbdf9434318ff
lucas barria <lucasbarria2002@gmail.com>
2020-11-10 16:38:43 -0300

    crear producto

--- ebooks/routes/index.js
+++ ebooks/routes/index.js
@@ -4,13 +4,9 @@ const indexController = require('../controllers/indexController');
 
 /* GET home page. */
 router.get('/', indexController.home);
-
 router.get('/paginadeproducto', indexController.productDetail);
-
 router.get('/registro', indexController.register);
-
 router.get('/iniciasesion', indexController.iniciasesion);
-
 router.get('/carrito', indexController.productCart);
 
 module.exports = router;
