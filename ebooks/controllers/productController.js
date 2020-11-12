1b179d72e88ad3d2934410d1e33e582f30377782
ulisesmorgulis <ulisesmorgulis.99@gmail.com>
2020-11-12 16:01:20 -0300

    products

--- ebooks/controllers/productController.js
+++ ebooks/controllers/productController.js
@@ -9,8 +9,14 @@ const productController = {
             imagen: req.body.img,
             categoria: req.body.categoria,
             precio: req.body.precio
-        }
-    }
+        } //tomar json de producto
+        //convertirlo a js
+        //pushear nuevo producto
+        //convertirlo a json
+        //sobrescribir el json
+        res.send ('producto creado')
+    } 
 }
 
 module.exports = productController;
+
