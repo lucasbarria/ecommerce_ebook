658291c719eea39a8ea7208d384d76aef8acbd86
ulises <ulisesmorgulis.99@gmail.com>
2021-01-28 16:59:52 -0300

    valid

--- ebooks/public/js/validaciones.js
+++ ebooks/public/js/validaciones.js
@@ -1,9 +1,24 @@
 window.addEventListener('load', function() {
-    const formulario = document.querySelector('form');
+    const form = document.querySelector('form');
 
-
+   /* form.addEventListener('blur', function(event) {
+        if(event.target.value.isAlpha){
+            console.log('error')
+        }
+    }) */
     
+    console.log(form)
 
-    formulario.addEventListener('submit', function(evento) {
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        let input = document.querySelectorAll('input');
+        for (let i = 0; i < input.length; i++){
+            if(input[i].value == ""){
+                console.log("no se puede enviar")
+            }
+        }
+
+
 })
 });
