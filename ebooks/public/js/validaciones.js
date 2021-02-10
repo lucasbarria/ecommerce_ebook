9f4ad630dd6163ba7ca86df387d55ddcb93bfca8
ulises <ulisesmorgulis.99@gmail.com>
2021-02-10 14:33:44 -0300

    vald

--- ebooks/public/js/validaciones.js
+++ ebooks/public/js/validaciones.js
@@ -6,7 +6,15 @@ window.addEventListener('load', function(){
     const password2 = document.getElementById('pass2');
     const date = document.getElementById('date');
     const genre = document.getElementsByName('genre');
+    const inputs = document.querySelectorAll('input');
 
+    const campos = {
+        name: false,
+        email: false,
+        password: false,
+        password2: false,
+        date: false,
+    }
 
     function setError(input, message) {
         const div = input.parentElement;
@@ -14,6 +22,7 @@ window.addEventListener('load', function(){
         const error = div.querySelector('p');
         error.innerText = message;
         error.className = 'error-activo';
+        campos[input] = false;
     }
 
     function setSucces(input){
@@ -21,57 +30,81 @@ window.addEventListener('load', function(){
         const error = div.querySelector('p');
         input.style.borderColor = '#10DF0D';
         error.className = 'error';
+        campos[input] = true;
     }
 
-    function checkInputs() {
+    function checkInputs(e) {
         const nameValue = name.value.trim();
         const emailValue = email.value.trim();
         const passwordValue = password.value.trim();
         const password2Value = password2.value.trim();
         const dateValue = date.value;
 
-        if(nameValue === ""){
-            setError(name, 'El nombre esta vacio');
-        } else {
-            setSucces(name);
-        }
+        switch (e.target.name){
+            case "name":
+                if(nameValue === ""){
+                    setError(name, 'El nombre esta vacio');
+                } else if (nameValue.length < 6){
+                    setError(name, 'Nombre invalido');
+                } else {
+                    setSucces(name);
+                }
+            break;
 
-        if(emailValue === ""){
-            setError(email, "El email esta vacio");
-        } else if (!isEmail(emailValue)){
-            setError(email, "Email invalido");
-        } else {
-            setSucces(email);
-        }
+            case "email":
+                if(emailValue === ""){
+                    setError(email, "El email esta vacio");
+                } else if (!isEmail(emailValue)){
+                    setError(email, "Email invalido");
+                } else {
+                    setSucces(email);
+                }
+            break;
 
-        if(passwordValue === ""){
-            setError(password, 'La contraseña esta vacia');
-        } else if (passwordValue.length < 8){
-            setError(password, 'La contraseña debe tener al menos 8 caracteres');
-        } else {
-            setSucces(password);
-        }
+            case "pass":
+                if(passwordValue === ""){
+                    setError(password, 'La contraseña esta vacia');
+                } else if (passwordValue.length < 8){
+                    setError(password, 'La contraseña debe tener al menos 8 caracteres');
+                } else {
+                    setSucces(password);
+                }
+            break;
 
-        if(password2Value === ""){
-            setError(password2, 'La contraseña esta vacia');
-        } else if (password2Value !== passwordValue){
-            setError(password2, 'Las contrasñeas no coinciden');
-        } else {
-            setSucces(password2);
-        }
+            case "pass2":
+                if(password2Value === ""){
+                    setError(password2, 'La contraseña esta vacia');
+                }else if (password2Value.length < 8){
+                    setError(password2, 'Las contraseñas deben tener al menos 8 caracteres');
+                } else if (password2Value !== passwordValue){
+                    setError(password2, 'Las contrasñeas no coinciden');
+                } else {
+                    setSucces(password2);
+                }
+            break;
 
-        if(dateValue === ""){
-            setError(date, 'La fecha esta incompleta');
-        } else {
-            setSucces(date);
+            case "date":
+                if(dateValue === ""){
+                    setError(date, 'La fecha esta incompleta');
+                } else {
+                    setSucces(date);
+                }
+            break;
         }
-        
     } 
+
+    inputs.forEach(function(input){
+        input.addEventListener('keyup', checkInputs);
+    })
 
     form.addEventListener('submit', function(e) {
         e.preventDefault();
         
-        checkInputs();
+        if(campos.name && campos.email && campos.password && campos.password2 && campos.date){
+            console.log("todo ok")
+        } else {
+            console.log("no se envia")
+        }
     });
 
     function isEmail(email) {
